092b84cbebb6f5588f98345a8ea19ed006cdacdf
stas <stas-fomin@yandex.ru>
2024-03-07 18:58:26 +0300

    add unicodeanyway

--- atomic_transformation/__init__.py
+++ atomic_transformation/__init__.py
@@ -4,4 +4,4 @@ __author__ = """Stas Fomin"""
 __email__ = 'stas-fomin@yandex.ru'
 __version__ = '0.1.0'
 
-from .atomic_transformation import transaction
+from .atomic_transformation import transaction, unicodeanyway
